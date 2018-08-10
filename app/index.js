6de2289e1d15a54b4d704c2b1c403ac9e6cdffe5
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-10 07:50:19 -0300

    Arrow Functions

--- app/index.js
+++ app/index.js
@@ -1,6 +1,13 @@
-let magical = true;
-let power = 2;
+// function blastoff(){
+//     console.log('3...2...1... blastoff!');
+// }
 
-let ranger = { magical: false, power: 9 };
-({ magical, power } = ranger);
-console.log(magical, power);
+// setTimeout(()=>{
+//     console.log('3...2...1... blastoff!');
+// },1000);
+
+const blastoff = () => {
+    console.log('3...2...1... blastoff!');
+}
+
+blastoff();

8dfd9066a402a2194544b9f1f80805aab47f990a
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-10 08:06:18 -0300

    Benefits of arrow functions (global this instead of local scope this)

--- app/index.js
+++ app/index.js
@@ -1,13 +1,12 @@
-// function blastoff(){
-//     console.log('3...2...1... blastoff!');
-// }
+this.a = 25;
 
-// setTimeout(()=>{
-//     console.log('3...2...1... blastoff!');
-// },1000);
-
-const blastoff = () => {
-    console.log('3...2...1... blastoff!');
+let print = function(){
+    this.a = 50;
+    console.log('this.a', this.a);
 }
 
-blastoff();
+let arrowPrint = () =>{
+    console.log('this.a in arrowPrint',this.a);
+}
+print();
+arrowPrint();

51bacaf20971d85cb7011c80be998cf734cbc2be
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-10 08:19:24 -0300

    Map exercise

--- app/index.js
+++ app/index.js
@@ -1,6 +1,17 @@
-let points = [10, 20, 30];
+// let points = [10, 20, 30];
 
 
-points = points.map(element => element + 1);
+// points = points.map(element => element + 1);
 
-console.log(points);
+// console.log(points);
+
+
+let lengths = [3, 7, 5];
+let multiple = 8;
+
+const scale = (multiple) => {
+    lengths.map(x => x*multiple);
+    
+  return lengths; // TODO fill out the rest;
+};
+scale(8);

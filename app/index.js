d54405639eee86d933cd3671354e80743071ce79
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-13 07:49:05 -0300

    Exporting Values

--- app/index.js
+++ app/index.js
@@ -1,9 +1,2 @@
-// let isPassing = (grade) => {
-//     return grade >= 90;
-// }
-
-let scores = [90, 85, 67, 71, 70, 55, 92];
-
-let passing = scores.filter(x => x >= 70);
-
-console.log(passing);
+import { students, total } from './students';
+console.log(students, total);

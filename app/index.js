ef14824f242b59f977fe5cb59d010d9a51040adf
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-13 07:58:16 -0300

    Exporting Expressions and by Default

--- app/index.js
+++ app/index.js
@@ -1,2 +1,5 @@
-import { students, total } from './students';
-console.log(students, total);
+// import { add, multiply } from './calculator.js';
+// console.log(add(1, 1));
+
+import multiply from './calculator';
+console.log(multiply(2, 2));

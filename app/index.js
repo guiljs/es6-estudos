d5fc766d36fea9164bbedfe88bf9e8e519eabed7
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-14 08:30:26 -0300

    Promises and more React - Sample with OpenWeather API

--- app/index.js
+++ app/index.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+const api_key = '92f8f0fbf240fc46079bafca7aa56c15';
+
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            city: 'São Paulo',
+            description: ''
+        }
+    }
+
+    componentDidMount() {
+        this.grabWeather(this.state.city);
+    }
+
+    grabWeather(city) {
+
+        fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
+            .then(response => response.json())
+            .then(json => {
+                this.setState({ description: json.weather[0].description })
+            });
+
+    }
+
     render() {
         return (
-            <div>React JS and JSX in action!</div>
+            <div>
+                <h1>Weather report for {this.state.city}! </h1>
+                <h2>{this.state.description}</h2>
+            </div>
         )
     }
 }
